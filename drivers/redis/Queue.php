b00ea5291fd476609cd5285823d4fe0b38ad33d1
Zhao vistart <i@vistart.me>
2019-01-13 02:06:05 +0800

    Create Queue.php

---
+++ drivers/redis/Queue.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ *  _   __ __ _____ _____ ___  ____  _____
+ * | | / // // ___//_  _//   ||  __||_   _|
+ * | |/ // /(__  )  / / / /| || |     | |
+ * |___//_//____/  /_/ /_/ |_||_|     |_|
+ * @link https://vistart.me/
+ * @copyright Copyright (c) 2016 - 2019 vistart
+ * @license https://vistart.me/license/
+ */
+
+namespace rhoone\spider\redis;
+
+/**
+ * Class Queue
+ * @package rhoone\spider\redis
+ */
+class Queue extends \yii\queue\redis\Queue
+{
+
+}
